210c42ed0eaf8d5ebb70e55c94dcd391a845271f
DamianVillalba <devillalba20@gmail.com>
2026-02-03 17:16:42 -0300

    feat: initial structure

---
+++ src/main/java/com/nocountry/equitrust/EquitrustApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.equitrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquitrustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquitrustApplication.class, args);
+	}
+
+}
