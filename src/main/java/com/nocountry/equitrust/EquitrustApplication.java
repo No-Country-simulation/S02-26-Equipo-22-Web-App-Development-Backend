23b71db74fd6cdc84fde8ae1c7b1927bacfcd962
DamianVillalba <devillalba20@gmail.com>
2026-02-12 17:58:57 -0300

    feat: horse entity created

--- src/main/java/com/nocountry/equitrust/EquitrustApplication.java
+++ src/main/java/com/nocountry/equitrust/EquitrustApplication.java
@@ -2,8 +2,10 @@ package com.nocountry.equitrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EquitrustApplication {
 
 	public static void main(String[] args) {
