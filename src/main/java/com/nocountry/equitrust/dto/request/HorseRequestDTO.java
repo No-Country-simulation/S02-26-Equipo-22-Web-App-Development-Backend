8f2527712661abf48827176095ed2cde5ae54799
evanghel1on <cfloresu@uni.pe>
2026-02-18 19:12:18 -0500

    feat(horse): added Swagger Documentation

--- src/main/java/com/nocountry/equitrust/dto/request/HorseRequestDTO.java
+++ src/main/java/com/nocountry/equitrust/dto/request/HorseRequestDTO.java
@@ -1,5 +1,6 @@
 package com.nocountry.equitrust.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -8,16 +9,21 @@ import java.math.BigDecimal;
 
 public record HorseRequestDTO(
 
+        @Schema(description = "Raza del caballo", example = "Andaluz")
         @NotBlank(message = "La raza es requerido")
         String breed,
 
+        @Schema(description = "Descripción detallada del caballo", example = "Un hermoso semental blanco, bien entrenado.")
         String description,
 
+        @Schema(description = "Ubicación actual del caballo", example = "Buenos Aires, Argentina")
         String location,
 
+        @Schema(description = "Precio de venta del caballo en dólares", example = "15000.00")
         @Positive(message = "El precio debe ser positivo")
         BigDecimal price,
 
+        @Schema(description = "ID del usuario dueño del caballo", example = "1")
         @NotNull(message = "El ID del dueño es requerido")
         Long ownerId
 ) {}
