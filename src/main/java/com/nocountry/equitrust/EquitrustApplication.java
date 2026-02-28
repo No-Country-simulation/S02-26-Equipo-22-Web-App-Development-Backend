296bcf2770790a76107c65634d0b2c37014df70f
Tomás Laus <tomaslaus99@gmail.com>
2026-02-28 19:17:09 -0300

    DB Debug

--- src/main/java/com/nocountry/equitrust/EquitrustApplication.java
+++ src/main/java/com/nocountry/equitrust/EquitrustApplication.java
@@ -2,10 +2,14 @@ package com.nocountry.equitrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = "com.nocountry.equitrust")
+@EnableJpaRepositories(basePackages = "com.nocountry.equitrust")
 public class EquitrustApplication {
 
 	public static void main(String[] args) {
